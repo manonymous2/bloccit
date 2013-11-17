462a555f72166bf8d20f37adcc100ee5ba7e99e0
manonymous2 <esther@manonymous.com>
2013-11-16 20:05:36 -0500

    created welcome controller with index and about views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Bloccit::Application.routes.draw do
+
+  get "welcome/index"
+
+  get "welcome/about"
+
+   resources :posts
+
+  match "about" => 'welcome#about', via: :get
+
+  root :to => 'welcome#index'
+  
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
