22511bce541914946de95d590fb890c2b049eda4
manonymous2 <esther@manonymous.com>
2013-11-17 20:23:05 -0500

    Added image to Post.

--- Gemfile
+++ Gemfile
@@ -20,6 +20,10 @@ gem 'newrelic_rpm'
 gem 'jquery-rails'
 gem 'cancan'
 gem 'redcarpet'
+gem 'carrierwave'
+gem 'mini_magick'
+gem 'fog'
+gem 'figaro'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131117043733) do
+ActiveRecord::Schema.define(:version => 20131118004000) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(:version => 20131117043733) do
     t.datetime "created_at",                                            :null => false
     t.datetime "updated_at",                                            :null => false
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
